094c877cec7ca539ec1bc06de6214e01a34b10ad
unknown <ashutoshpatil35@gmail.com>
2025-07-15 20:18:01 +0530

    s3 resource created

--- documents/terraform/main.tf
+++ documents/terraform/main.tf
@@ -6,18 +6,39 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.3.0"
     }
-    google = {
-      source  = "hashicorp/google"
-      version = "6.43.0"
-    }
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.36.0"
-    }
-
   }
 }
 
 provider "aws" {
   region = "ap-south-1" // AWS region
 }
+
+// Resource blocks define the infrastructure components
+// Example: Create an S3 bucket
+resource "aws_s3_bucket" "bucket-1" {
+  // 1st resrource block
+  bucket = "terraform-created-bucket-1507" // Unique bucket name
+
+  tags = {
+    createdBy   = "Terraform" // update ~
+    environment = "dev"       //add + 
+  }
+}
+
+
+resource "aws_s3_bucket" "bucket-2" {
+  // 2nd resource block
+  bucket = "terraform-created-awesomebucket" // Unique bucket name
+
+  tags = {
+    createdBy   = "Terraform"
+    environment = "dev"
+  }
+}
+
+
+
+# .aws> cat .\credentials
+# [default]
+# aws_access_key_id = 
+# aws_secret_access_key = 
